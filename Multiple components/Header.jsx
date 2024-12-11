6fd8d1ce62d9addb5274f03f75337341e5476121
Manish <heymanishthakur@gmail.com>
2024-12-12 05:22:13 +0530

    multiple components

---
+++ Multiple components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>I am learning React</h1>
+      <nav>
+        <a href="">HOME</a>
+        <a href="">CONTACT</a>
+        <a href="">ABOUT</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
